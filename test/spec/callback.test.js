43d43c5c4c24eec3724260d2f47996493852212d
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-25 00:55:12 -0800

    Upgrade modules

--- test/spec/callback.test.js
+++ test/spec/callback.test.js
@@ -1,131 +1,131 @@
-var assert = require('assert');
+const assert = require('assert');
 
-var toVersion = require('../..');
+const toVersion = require('version-string-to-version');
 
-describe('callback', function () {
-  describe('happy path', function () {
-    it('v12', function (done) {
-      toVersion('v12', function (err, version) {
-        assert.ok(!err);
+describe('callback', () => {
+  describe('happy path', () => {
+    it('v12', (done) => {
+      toVersion('v12', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.equal(version.slice(0, 4), 'v12.');
         done();
       });
     });
-    it('12', function (done) {
-      toVersion('12', function (err, version) {
-        assert.ok(!err);
+    it('12', (done) => {
+      toVersion('12', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.equal(version.slice(0, 4), 'v12.');
         done();
       });
     });
-    it('v0', function (done) {
-      toVersion('v0', function (err, version) {
-        assert.ok(!err);
+    it('v0', (done) => {
+      toVersion('v0', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.equal(version.slice(0, 3), 'v0.');
         done();
       });
     });
-    it('0', function (done) {
-      toVersion('0', function (err, version) {
-        assert.ok(!err);
+    it('0', (done) => {
+      toVersion('0', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.equal(version.slice(0, 3), 'v0.');
         done();
       });
     });
-    it('v12.0', function (done) {
-      toVersion('v12.0', function (err, version) {
-        assert.ok(!err);
+    it('v12.0', (done) => {
+      toVersion('v12.0', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.equal(version.slice(0, 6), 'v12.0.');
         done();
       });
     });
-    it('12.0', function (done) {
-      toVersion('12.0', function (err, version) {
-        assert.ok(!err);
+    it('12.0', (done) => {
+      toVersion('12.0', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.equal(version.slice(0, 6), 'v12.0.');
         done();
       });
     });
-    it('v12.1.0', function (done) {
-      toVersion('v12.1.0', function (err, version) {
-        assert.ok(!err);
+    it('v12.1.0', (done) => {
+      toVersion('v12.1.0', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.equal(version, 'v12.1.0');
         done();
       });
     });
-    it('12.1.0', function (done) {
-      toVersion('12.1.0', function (err, version) {
-        assert.ok(!err);
+    it('12.1.0', (done) => {
+      toVersion('12.1.0', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.equal(version, 'v12.1.0');
         done();
       });
     });
-    it('>=8', function (done) {
-      toVersion('>=8', function (err, version) {
-        assert.ok(!err);
+    it('>=8', (done) => {
+      toVersion('>=8', (err, version) => {
+        assert.ok(!err, err ? err.message : '');
         assert.ok(version.length > 1);
         done();
       });
     });
   });
 
-  describe('unhappy path', function () {
-    it('v0.0', function (done) {
-      toVersion('v0.0', function (err) {
+  describe('unhappy path', () => {
+    it('v0.0', (done) => {
+      toVersion('v0.0', (err) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('0.0', function (done) {
-      toVersion('0.0', function (err) {
+    it('0.0', (done) => {
+      toVersion('0.0', (err) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('v0.0.0', function (done) {
-      toVersion('v0.0.0', function (err, version) {
+    it('v0.0.0', (done) => {
+      toVersion('v0.0.0', (err, _version) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('0.0.0', function (done) {
-      toVersion('0.0.0', function (err, version) {
+    it('0.0.0', (done) => {
+      toVersion('0.0.0', (err, _version) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('va.0.1', function (done) {
-      toVersion('va.0.1', function (err) {
+    it('va.0.1', (done) => {
+      toVersion('va.0.1', (err) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('v12a.0.1', function (done) {
-      toVersion('v12a.0.1', function (err) {
+    it('v12a.0.1', (done) => {
+      toVersion('v12a.0.1', (err) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('v12.b.1', function (done) {
-      toVersion('v12.b.1', function (err) {
+    it('v12.b.1', (done) => {
+      toVersion('v12.b.1', (err) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('v12.0b.1', function (done) {
-      toVersion('v12.0b.1', function (err) {
+    it('v12.0b.1', (done) => {
+      toVersion('v12.0b.1', (err) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('v12.0.c', function (done) {
-      toVersion('v12.0.c', function (err) {
+    it('v12.0.c', (done) => {
+      toVersion('v12.0.c', (err) => {
         assert.ok(!!err);
         done();
       });
     });
-    it('v12.1.0c', function (done) {
-      toVersion('v12.1.0c', function (err) {
+    it('v12.1.0c', (done) => {
+      toVersion('v12.1.0c', (err) => {
         assert.ok(!!err);
         done();
       });
