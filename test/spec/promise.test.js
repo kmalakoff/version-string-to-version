bbb0dcd82675e48bc76e61b9cac5e91bb20b564f
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 09:35:41 -0800

    Upgrade modules

--- test/spec/promise.test.js
+++ test/spec/promise.test.js
@@ -3,181 +3,135 @@ const assert = require('assert');
 const toVersion = require('version-string-to-version');
 
 describe('promise', () => {
+  const root = typeof global !== 'undefined' ? global : window;
+  let rootPromise;
+  before(() => {
+    rootPromise = root.Promise;
+    root.Promise = require('pinkie-promise');
+  });
+  after(() => {
+    root.Promise = rootPromise;
+  });
+
   describe('happy path', () => {
-    it('v12', (done) => {
-      toVersion('v12')
-        .then((version) => {
-          assert.equal(version.slice(0, 4), 'v12.');
-          done();
-        })
-        .catch(done);
+    it('v12', async () => {
+      const version = await toVersion('v12');
+      assert.equal(version.slice(0, 4), 'v12.');
     });
-    it('12', (done) => {
-      toVersion('12')
-        .then((version) => {
-          assert.equal(version.slice(0, 4), 'v12.');
-          done();
-        })
-        .catch(done);
+    it('12', async () => {
+      const version = await toVersion('12');
+      assert.equal(version.slice(0, 4), 'v12.');
     });
-    it('v0', (done) => {
-      toVersion('v0')
-        .then((version) => {
-          assert.equal(version.slice(0, 3), 'v0.');
-          done();
-        })
-        .catch(done);
+    it('v0', async () => {
+      const version = await toVersion('v0');
+      assert.equal(version.slice(0, 3), 'v0.');
     });
-    it('0', (done) => {
-      toVersion('0')
-        .then((version) => {
-          assert.equal(version.slice(0, 3), 'v0.');
-          done();
-        })
-        .catch(done);
+    it('0', async () => {
+      const version = await toVersion('0');
+      assert.equal(version.slice(0, 3), 'v0.');
     });
-    it('v12.0', (done) => {
-      toVersion('v12.0')
-        .then((version) => {
-          assert.equal(version.slice(0, 6), 'v12.0.');
-          done();
-        })
-        .catch(done);
+    it('v12.0', async () => {
+      const version = await toVersion('v12.0');
+      assert.equal(version.slice(0, 6), 'v12.0.');
     });
-    it('12.0', (done) => {
-      toVersion('12.0')
-        .then((version) => {
-          assert.equal(version.slice(0, 6), 'v12.0.');
-          done();
-        })
-        .catch(done);
+    it('12.0', async () => {
+      const version = await toVersion('12.0');
+      assert.equal(version.slice(0, 6), 'v12.0.');
     });
-    it('v12.1.0', (done) => {
-      toVersion('v12.1.0')
-        .then((version) => {
-          assert.equal(version, 'v12.1.0');
-          done();
-        })
-        .catch(done);
+    it('v12.1.0', async () => {
+      const version = await toVersion('v12.1.0');
+      assert.equal(version, 'v12.1.0');
     });
-    it('12.1.0', (done) => {
-      toVersion('12.1.0')
-        .then((version) => {
-          assert.equal(version, 'v12.1.0');
-          done();
-        })
-        .catch(done);
+    it('12.1.0', async () => {
+      const version = await toVersion('12.1.0');
+      assert.equal(version, 'v12.1.0');
     });
-    it('>=8', (done) => {
-      toVersion('>=8')
-        .then((version) => {
-          assert.ok(version.length > 1);
-          done();
-        })
-        .catch(done);
+    it('>=8', async () => {
+      const version = await toVersion('>=8');
+      assert.ok(version.length > 1);
     });
   });
 
   describe('unhappy path', () => {
-    it('v0.0', (done) => {
-      toVersion('v0.0')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('v0.0', async () => {
+      try {
+        await toVersion('v0.0');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('0.0', (done) => {
-      toVersion('0.0')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('0.0', async () => {
+      try {
+        await toVersion('0.0');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('v0.0.0', (done) => {
-      toVersion('v0.0.0')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('v0.0.0', async () => {
+      try {
+        await toVersion('v0.0.0');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('0.0.0', (done) => {
-      toVersion('0.0.0')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('0.0.0', async () => {
+      try {
+        await toVersion('0.0.0');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('va.0.1', (done) => {
-      toVersion('va.0.1')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('va.0.1', async () => {
+      try {
+        await toVersion('va.0.1');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('v12a.0.1', (done) => {
-      toVersion('v12a.0.1')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('v12a.0.1', async () => {
+      try {
+        await toVersion('v12a.0.1');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('v12.b.1', (done) => {
-      toVersion('v12.b.1')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('v12.b.1', async () => {
+      try {
+        await toVersion('v12.b.1');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('v12.0b.1', (done) => {
-      toVersion('v12.0b.1')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('v12.0b.1', async () => {
+      try {
+        await toVersion('v12.0b.1');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('v12.0.c', (done) => {
-      toVersion('v12.0.c')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('v12.0.c', async () => {
+      try {
+        await toVersion('v12.0.c');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
-    it('v12.1.0c', (done) => {
-      toVersion('v12.1.0c')
-        .then(() => {
-          assert.ok(false);
-        })
-        .catch((err) => {
-          assert.ok(!!err);
-          done();
-        });
+    it('v12.1.0c', async () => {
+      try {
+        await toVersion('v12.1.0c');
+        assert.ok(false);
+      } catch (err) {
+        assert.ok(!!err);
+      }
     });
   });
 });
