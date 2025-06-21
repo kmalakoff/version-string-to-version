e92106e9658d86bd9d2778acf4195071dae752d2
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 13:46:42 -0700

    Comprehensive types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-import worker from './worker.cjs';
+import type { VersionCallback, VersionOptions, VersionResult } from './types.ts';
+import worker from './worker.ts';
 
-export default function versionStringToVersion(versionString, options, callback) {
+export type * from './types.ts';
+
+export default function resolveVersion(versionString: string): Promise<VersionResult[]>;
+export default function resolveVersion(versionString: string, options: VersionOptions): Promise<VersionResult[]>;
+
+export default function resolveVersion(versionString: string, callback: VersionCallback): undefined;
+export default function resolveVersion(versionString: string, options: VersionOptions, callback: VersionCallback): undefined;
+
+export default function resolveVersion(versionString: string, options?: VersionOptions | VersionCallback, callback?: VersionCallback): undefined | Promise<VersionResult[]> {
   if (typeof options === 'function') {
-    callback = options;
+    callback = options as VersionCallback;
     options = {};
   }
   options = options || {};
 
-  if (typeof callback === 'function') return worker(versionString, options, callback);
-  return new Promise((resolve, reject) => worker(versionString, options, (err, result) => (err ? reject(err) : resolve(result))));
+  if (typeof callback === 'function') return worker(versionString, options, callback) as undefined;
+  return new Promise((resolve, reject) =>
+    worker(versionString, options, (err, result) => {
+      err ? reject(err) : resolve(result);
+    })
+  );
 }
