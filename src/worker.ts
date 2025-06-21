8470dc686af52b6f320d59945174554a48f3665b
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 13:46:42 -0700

    Comprehensive types

--- src/worker.ts
+++ src/worker.ts
@@ -1,11 +1,14 @@
-const NodeSemvers = require('node-semvers');
+import NodeSemvers from 'node-semvers';
+
 const isArray = Array.isArray || ((x) => Object.prototype.toString.call(x) === '[object Array]');
 
-module.exports = function resolveVersion(versionString, options, callback) {
+import type { VersionCallback, VersionOptions, VersionResult } from './types.ts';
+
+export default function resolveVersion(versionString: string, options: VersionOptions, callback: VersionCallback): undefined {
   NodeSemvers.load((err, semvers) => {
     if (err) return callback(err);
-    const version = semvers.resolve(versionString, options);
+    const version = semvers.resolve(versionString, options) as VersionResult[];
     if ((isArray(version) && !version.length) || !version) return callback(new Error(`Version string invalid ${versionString}`));
     callback(null, version);
   });
-};
+}
