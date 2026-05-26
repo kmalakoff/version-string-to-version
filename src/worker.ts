0f842b828ee5ac0ddb3f373c68568ac43dd5bc7a
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 08:56:18 -0700

    Fix broken types

--- src/worker.ts
+++ src/worker.ts
@@ -7,8 +7,8 @@ import type { VersionCallback, VersionOptions, VersionResult } from './types.ts'
 export default function resolveVersion(versionString: string, options: VersionOptions, callback: VersionCallback): void {
   NodeSemvers.load((err, semvers) => {
     if (err) return callback(err);
-    const version = semvers.resolve(versionString, options) as VersionResult[];
+    const version = semvers?.resolve(versionString, options) as VersionResult[];
     if ((isArray(version) && !version.length) || !version) return callback(new Error(`Version string invalid ${versionString}`));
-    callback(null, version);
+    callback(undefined, version);
   });
 }
